c20b1bee8049c223c98d8bb0cd74bb4297368486
tanguygalliffet <galliffettpro@gmail.com>
2025-02-06 11:02:25 +0100

    first commit

---
+++ src/main/java/fr/estia/tp_microservices/TpMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package fr.estia.tp_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpMicroservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/estia/tp_microservices/TpMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.estia.tp_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
